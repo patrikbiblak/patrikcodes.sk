2bafa32f92136fc512e60b6bd33d024f6554b25a
patrik <patrik.biblak@gmail.com>
2025-07-10 16:23:35 +0200

    preložený nadpis do sk

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -3,7 +3,7 @@ import "../styles/projectspage.css";
 const ProjectsPage = () => {
     return (
         <div className="projects-page">
-            <h1>Projects</h1>
+            <h1>Moje projekty</h1>
         </div>
     )
 }

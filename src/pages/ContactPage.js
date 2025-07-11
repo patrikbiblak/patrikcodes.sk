bda41a2737b1c43d540b6a8d1299547307fccb9e
patrik <patrik.biblak@gmail.com>
2025-07-11 15:23:46 +0200

    google-map added

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -90,6 +90,16 @@ const ContactPage = () => {
             })}
           </div>
         </div>
+
+            <div className="map-container">
+                <iframe
+                title="Mapa"
+                src="https://maps.google.com/maps?q=Slovensko&output=embed"
+                allowFullScreen
+                loading="lazy"
+                style={{ border: 0, width: '100%', height: '100%' }}
+                />
+            </div>
         </div>
   );
 };

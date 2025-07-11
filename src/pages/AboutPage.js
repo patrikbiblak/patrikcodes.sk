ca0a2942ef4169f47d4f6db5032451ac0136e57e
patrik <patrik.biblak@gmail.com>
2025-07-11 09:51:20 +0200

    added content for about subpage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,9 +2,23 @@ import "../styles/aboutpage.css";
 
 const AboutPage = () => {
     return (
-        <div className="about-page">
-            <h1>Kto som?</h1>
-            <p></p>
+        <div className="about-page container">
+            <div className="aboutme-heading">
+                <h1>Kto som?</h1>
+                <p>Som mladý a nadšený front-end developer ktorý kladie dôraz na transparentnú komunikáciu, dodržiavanie termínov a vysokú kvalitu.</p>
+                <p>Vytváram rýchle a responzívne webové stránky presne podľa vašich predstáv, ktoré plynulo fungujú na rôznych zariadeniach. Môžete sa na mňa spoľahnúť, že vaše nápady premením na pútavú online prezentáciu, ktorá osloví vašich návštevníkov.</p>
+                <p>Potrebujete niekoho, kto zvládne aj developerské šprinty? Zavolajte ma!</p>
+            </div>
+
+            <div className="aboutme-text">
+                <img src="" alt="" />
+                <h3>@bpaatrik</h3>
+                <p>Káva, kód a disciplína tvoria môj dokonalý trojboj. Do každého projektu vkladám stovku percent, aby výsledok bežal hladko a bez bugov. Po práci ma šport udrží v kondícii a pomáha mi udržať si čistú myseľ.</p>
+            </div>
+
+            <div className="aboutme-skills">
+                
+            </div>
         </div>
     )
 }
